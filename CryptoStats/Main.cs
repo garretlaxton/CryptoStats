aa1cf4bb258b1b56c330022e5562784ba5c3d721
garretlaxton <garretlaxton@gmail.com>
2018-02-08 16:39:53 -0500

    Packages & dependencies

--- CryptoStats/Main.cs
+++ CryptoStats/Main.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
