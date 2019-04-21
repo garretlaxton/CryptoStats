624e2ec6901f6991245ed4d28d681a1413e68fde
garretlaxton <garretlaxton@gmail.com>
2019-04-21 04:38:28 -0400

    Copyright year

--- CryptoStats/Properties/AssemblyInfo.cs
+++ CryptoStats/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CryptoStats")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright ©  2018 - 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
